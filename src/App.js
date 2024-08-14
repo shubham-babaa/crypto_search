5ce2f629dd08d1edb966c14b7f5dff9d8ce1d55d
shubham-babaa <manshipatel.deep@gmail.com>
2024-08-14 11:37:11 +0530

    feat: add apis+ readme

--- src/App.js
+++ src/App.js
@@ -10,6 +10,7 @@ function App() {
 
   const [coins, setCoins] = useState([])
 
+  
   const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false'
 
   useEffect(() => {
